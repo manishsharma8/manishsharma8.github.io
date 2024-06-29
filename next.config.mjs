8deae188cb439ea041b258ec30e1a21bf0571c40
manishsharma8 <sharmamanish8301@gmail.com>
2024-06-29 23:02:48 +0530

    fix: github pages action

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/2048-in-react',
   output: 'export',
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
-  assetPrefix: './',
+  reactStrictMode: true,
 };
 
 export default nextConfig;
